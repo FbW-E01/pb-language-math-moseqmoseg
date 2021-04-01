8e43a20daf6d74b7e50f4dfac8fe809b1042d2de
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-04-01 15:19:08 +0200

    Completed

---
+++ start.js
@@ -0,0 +1,17 @@
+console.log(Math.min(-1, 4));
+
+console.log(Math.round(3321.32321));
+console.log(Math.round(326.76));
+console.log(Math.round(76788.7));
+console.log(Math.round(-9.78));
+console.log(Math.round(43.342));
+
+
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+
+
+console.log(Math.floor(Math.random() * 6) + 1);
